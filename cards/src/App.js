c3ed91a9920028ec287dfefb82bea26fa2d8f30c
suzenhalil <suzenhalil@hotmail.com>
2023-07-10 22:13:35 +0300

    ..

--- cards/src/App.js
+++ cards/src/App.js
@@ -1,3 +1,4 @@
+import 'bulma/css/bulma.css';
 import './App.css';
 import  Course  from "./Course";
 import Angular from './images/angular.jpg';
@@ -10,12 +11,40 @@ import Kompleweb from './images/kompleweb.jpg';
 function App() {
   return (
     <div className="App">
-      {/* <img src={Angular} alt=''/> */}
+      <section className="hero is-link">
+  <div className="hero-body">
+    <p className="title">
+      Kurslarım
+    </p>
+    
+  </div>
+</section>
+      <div className='container'>
+        <section className='section'>
+        <div className='columns'>
+          <div className='cloumn'>
+          <Course image={Angular} title='Angular' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur volutpat, lectus sed elementum gravida, mauris nibh vestibulum tortor, vel mattis massa ligula ac mi. Curabitur sed quam fermentum, rhoncus ipsum a, aliquet nibh. Vestibulum faucibus ipsum dolor, pharetra dictum enim scelerisque eget. Pellentesque aliquet mi est, et rhoncus ex tristique eu. Morbi malesuada nisi sed purus mattis congue. Maecenas nec erat id ex tempus gravida a a nisi. Ut a vulputate nisl, at volutpat diam. Vivamus commodo dui nec arcu finibus dapibus. Proin facilisis ultricies molestie. Quisque non tellus non nulla tincidunt ornare id et sapien. Nullam eget posuere velit. Aliquam ultrices, erat non aliquam consequat, sapien turpis posuere ligula, nec bibendum velit lorem ornare orci. Ut eleifend mi nec libero faucibus tincidunt.'/>
+
+          </div>
+          <div className='cloumn'>
+          <Course image={Bootstrap} title='Bootstrap 5' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur volutpat, lectus sed elementum gravida, mauris nibh vestibulum tortor, vel mattis massa ligula ac mi. Curabitur sed quam fermentum, rhoncus ipsum a, aliquet nibh. Vestibulum faucibus ipsum dolor, pharetra dictum enim scelerisque eget. Pellentesque aliquet mi est, et rhoncus ex tristique eu. Morbi malesuada nisi sed purus mattis congue. Maecenas nec erat id ex tempus gravida a a nisi. Ut a vulputate nisl, at volutpat diam. Vivamus commodo dui nec arcu finibus dapibus. Proin facilisis ultricies molestie. Quisque non tellus non nulla tincidunt ornare id et sapien. Nullam eget posuere velit. Aliquam ultrices, erat non aliquam consequat, sapien turpis posuere ligula, nec bibendum velit lorem ornare orci. Ut eleifend mi nec libero faucibus tincidunt.'/>
+
+          </div>
+          <div className='cloumn'>
+          <Course image={Csharp} title='Komple Web' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur volutpat, lectus sed elementum gravida, mauris nibh vestibulum tortor, vel mattis massa ligula ac mi. Curabitur sed quam fermentum, rhoncus ipsum a, aliquet nibh. Vestibulum faucibus ipsum dolor, pharetra dictum enim scelerisque eget. Pellentesque aliquet mi est, et rhoncus ex tristique eu. Morbi malesuada nisi sed purus mattis congue. Maecenas nec erat id ex tempus gravida a a nisi. Ut a vulputate nisl, at volutpat diam. Vivamus commodo dui nec arcu finibus dapibus. Proin facilisis ultricies molestie. Quisque non tellus non nulla tincidunt ornare id et sapien. Nullam eget posuere velit. Aliquam ultrices, erat non aliquam consequat, sapien turpis posuere ligula, nec bibendum velit lorem ornare orci. Ut eleifend mi nec libero faucibus tincidunt.'/>
+
+          </div>
+          <div className='cloumn'>
+          <Course image={Kompleweb} title='Frontend' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur volutpat, lectus sed elementum gravida, mauris nibh vestibulum tortor, vel mattis massa ligula ac mi. Curabitur sed quam fermentum, rhoncus ipsum a, aliquet nibh. Vestibulum faucibus ipsum dolor, pharetra dictum enim scelerisque eget. Pellentesque aliquet mi est, et rhoncus ex tristique eu. Morbi malesuada nisi sed purus mattis congue. Maecenas nec erat id ex tempus gravida a a nisi. Ut a vulputate nisl, at volutpat diam. Vivamus commodo dui nec arcu finibus dapibus. Proin facilisis ultricies molestie. Quisque non tellus non nulla tincidunt ornare id et sapien. Nullam eget posuere velit. Aliquam ultrices, erat non aliquam consequat, sapien turpis posuere ligula, nec bibendum velit lorem ornare orci. Ut eleifend mi nec libero faucibus tincidunt.'/>
+
+          </div>
+
+        </div>
+        </section>
+        
+      </div>
       
-     <Course image={Angular} title='Angular' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur volutpat, lectus sed elementum gravida, mauris nibh vestibulum tortor, vel mattis massa ligula ac mi. Curabitur sed quam fermentum, rhoncus ipsum a, aliquet nibh. Vestibulum faucibus ipsum dolor, pharetra dictum enim scelerisque eget. Pellentesque aliquet mi est, et rhoncus ex tristique eu. Morbi malesuada nisi sed purus mattis congue. Maecenas nec erat id ex tempus gravida a a nisi. Ut a vulputate nisl, at volutpat diam. Vivamus commodo dui nec arcu finibus dapibus. Proin facilisis ultricies molestie. Quisque non tellus non nulla tincidunt ornare id et sapien. Nullam eget posuere velit. Aliquam ultrices, erat non aliquam consequat, sapien turpis posuere ligula, nec bibendum velit lorem ornare orci. Ut eleifend mi nec libero faucibus tincidunt.'/>
-     <Course image={Bootstrap} title='Bootstrap 5' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur volutpat, lectus sed elementum gravida, mauris nibh vestibulum tortor, vel mattis massa ligula ac mi. Curabitur sed quam fermentum, rhoncus ipsum a, aliquet nibh. Vestibulum faucibus ipsum dolor, pharetra dictum enim scelerisque eget. Pellentesque aliquet mi est, et rhoncus ex tristique eu. Morbi malesuada nisi sed purus mattis congue. Maecenas nec erat id ex tempus gravida a a nisi. Ut a vulputate nisl, at volutpat diam. Vivamus commodo dui nec arcu finibus dapibus. Proin facilisis ultricies molestie. Quisque non tellus non nulla tincidunt ornare id et sapien. Nullam eget posuere velit. Aliquam ultrices, erat non aliquam consequat, sapien turpis posuere ligula, nec bibendum velit lorem ornare orci. Ut eleifend mi nec libero faucibus tincidunt.'/>
-     <Course image={Csharp} title='Komple Web' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur volutpat, lectus sed elementum gravida, mauris nibh vestibulum tortor, vel mattis massa ligula ac mi. Curabitur sed quam fermentum, rhoncus ipsum a, aliquet nibh. Vestibulum faucibus ipsum dolor, pharetra dictum enim scelerisque eget. Pellentesque aliquet mi est, et rhoncus ex tristique eu. Morbi malesuada nisi sed purus mattis congue. Maecenas nec erat id ex tempus gravida a a nisi. Ut a vulputate nisl, at volutpat diam. Vivamus commodo dui nec arcu finibus dapibus. Proin facilisis ultricies molestie. Quisque non tellus non nulla tincidunt ornare id et sapien. Nullam eget posuere velit. Aliquam ultrices, erat non aliquam consequat, sapien turpis posuere ligula, nec bibendum velit lorem ornare orci. Ut eleifend mi nec libero faucibus tincidunt.'/>
-     <Course image={Kompleweb} title='Frontend' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur volutpat, lectus sed elementum gravida, mauris nibh vestibulum tortor, vel mattis massa ligula ac mi. Curabitur sed quam fermentum, rhoncus ipsum a, aliquet nibh. Vestibulum faucibus ipsum dolor, pharetra dictum enim scelerisque eget. Pellentesque aliquet mi est, et rhoncus ex tristique eu. Morbi malesuada nisi sed purus mattis congue. Maecenas nec erat id ex tempus gravida a a nisi. Ut a vulputate nisl, at volutpat diam. Vivamus commodo dui nec arcu finibus dapibus. Proin facilisis ultricies molestie. Quisque non tellus non nulla tincidunt ornare id et sapien. Nullam eget posuere velit. Aliquam ultrices, erat non aliquam consequat, sapien turpis posuere ligula, nec bibendum velit lorem ornare orci. Ut eleifend mi nec libero faucibus tincidunt.'/>
+      
     
     
     </div>
